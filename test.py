bb8ecb22267479c782c9fa9356974c3be9633016
Rodrigue Roland CODO <codorol22@gmail.com>
2022-11-01 18:25:12 +0500

    -

--- test.py
+++ test.py
@@ -1,19 +1,59 @@
+def get_min(d = None):
+    index = 1
+    mini = d[1]['ra']
+    for i in d :
+        if d[i]['ra'] < mini:
+            mini = d[i]['ra']
+            index = i
+    return index
 
-s = {1,2,3,4,5}
-j = {9,10,11,12}
-k = {5,6,7,8,9}
-data = []
-for _ in range(3):
-	a = [int(x) for x in input().split()]
-	a.pop(0)
-	data.append(set(a))
-print(sorted(data[0]|data[1]|data[2]))
-# s = 0
-# with open('input.txt','r') as f:
-# 	content = f.readlines()
-# 	for line in content:
-# 		line = [int(x) for x in line.split()]
-# 		s += sum(line)
+def get_max(d = None):
+    index = 1
+    maxi = d[1]['ra']
+    for i in d :
+        if d[i]['ra'] > maxi:
+            maxi = d[i]['ra']
+            index = i
+    return index
 
-# with open('output.txt','w') as f:
-# 	f.write(str(s))
+
+n , m , q = list(map(int,input().split()))
+
+datacenters = dict()
+
+for i in range(1,n+1):
+    datacenters[i] = {
+        'r' : 0,
+        'a' : [j for j in range(1,m+1)],
+        'm' : m,
+        'ra' : 0
+	}
+    
+results = []
+for _ in range(q):
+    event = input().split()
+    if event[0] == 'GETMIN':
+        results.append(get_min(datacenters))
+    elif event[0] == 'GETMAX':
+        results.append(get_max(datacenters))
+    elif event[0] == 'RESET':
+        i = int(event[1])
+        datacenters[i]['r'] += 1
+        datacenters[i]['a'] =[j for j in range(1,m+1)]
+        datacenters[i]['m'] = m
+        datacenters[i]['ra'] = datacenters[i]['r'] * datacenters[i]['m']
+    elif event[0] == 'DISABLE':
+        i,j = int(event[1]), int(event[2])
+        if j in datacenters[i]['a']:
+            datacenters[i]['a'].remove(j)
+            datacenters[i]['m'] -= 1
+            datacenters[i]['ra'] = datacenters[i]['r'] * datacenters[i]['m']
+    
+for r in results:
+    print(r)
+    
+# datacenter = {
+#     'm' : 3,
+#     'a' : 0,
+    
+# }
