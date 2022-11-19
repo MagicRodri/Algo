116dac2cce35a9d784d3d71980bcf98aacdeedde
Rodrigue CODO <codorol22@gmail.com>
2022-11-19 20:17:53 +0500

    Asyncio snippets

---
+++ try-asyncio/main.py
@@ -0,0 +1,14 @@
+import asyncio
+import time
+
+async def say_after(delay, what):
+    await asyncio.sleep(delay)
+    print(what)
+
+async def main():
+	print('Started at %s' %time.strftime('%X'))
+	await say_after(1,'Hello')
+	await say_after(2,'world')
+	print('Finished at %s' %time.strftime('%X'))
+
+asyncio.run(main())
