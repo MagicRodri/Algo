4bd8156399dd6612ef8259e121a76f13abf43e0a
Rodrigue Roland CODO <codorol22@gmail.com>
2022-12-27 12:54:17 +0500

    Renamed main.py to twitter.py

---
+++ twitter_bots/twitter.py
@@ -0,0 +1,15 @@
+import tweepy
+
+import config
+
+try:
+    client = tweepy.Client(
+        consumer_key=config.TW_CONSUMER_KEY,
+        consumer_secret=config.TW_CONSUMER_KEY_SECRET,
+        access_token=config.TW_ACCESS_TOKEN,
+        access_token_secret=config.TW_ACCESS_TOKEN_SECRET,
+    )
+except Exception as e:
+    print(e)
+
+print(client.get_home_timeline())
