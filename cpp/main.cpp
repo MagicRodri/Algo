e3abca89294a8c76d6eda79c32e5c921be331edf
Rodrigue CODO <codorol22@gmail.com>
2023-03-28 21:27:14 +0500

    cpp relearning ...

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
-#include <map>
+#include <ctime>
 
 int main()
 {
-    std::map<std::string, std::string> dict;
-    dict["hello"] = "world";
-    dict["foo"] = "bar";
-    for(auto& pair : dict)
-        std::cout << pair.first << " " << pair.second << std::endl;
+    int num;
+    int guess;
+    int tries = 0;
+    int number_range = 100;
+    srand(time(0));
+    num = rand() % number_range + 1;
+    std::cout << "Guess My Number Game" << std::endl;
+    do
+    {
+        std::cout << "Enter a guess between 1 and " << number_range << ": ";
+        std::cin >> guess;
+        tries++;
+        if (guess > num)
+        {
+            std::cout << "Too high!" << std::endl;
+        }
+        else if (guess < num)
+        {
+            std::cout << "Too low!" << std::endl;
+        }
+        else
+        {
+            std::cout << "Correct! You got it in " << tries << " guesses!" << std::endl;
+        }
+    } while (guess != num);
     return 0;
 }
