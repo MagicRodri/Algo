24b44e3d1befa0307175ff4d9eea647f1f466fe5
Rodrigue CODO <codorol22@gmail.com>
2023-06-14 11:35:46 +0500

    Youtube videos transcription

--- openai/transcription.py
+++ openai/transcription.py
@@ -1,5 +1,28 @@
-import whisper
+import logging
+from pathlib import Path
 
-model = whisper.load_model("base")
-result = model.transcribe("audio_fr.mp3")
-print(result["text"])
+import whisper
+from pytube import YouTube
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    transcriptions_base_path = Path("transcriptions")
+    # url = "https://www.youtube.com/watch?v=dCxSsr5xuL8"
+    url = "https://www.youtube.com/watch?v=8lGpZkjnkt4"
+    yt = YouTube(url)
+    title = yt.title
+    audio_stream = yt.streams.filter(only_audio=True).first()
+    filename = f'{title}.mp3'
+    output_path = transcriptions_base_path / title
+    audio_stream.download(filename=filename,output_path=str(output_path))
+
+    model = whisper.load_model("base")
+    result = model.transcribe(str(output_path / filename))
+    with open(output_path / f'{title}.txt','w') as f:
+        f.writelines(result['text'])
+
+
+if __name__ == '__main__':
+    main()
